2991b4303016e16e1e1ed36eea9a577fb6175725
Caden Rothzeid <cadenrothzeid@gmail.com>
2021-11-19 08:49:47 -0700

    now has edge detection need to scale back how much is white though so pytesseract can read the words better

--- main.py
+++ main.py
@@ -1,31 +1,39 @@
-# import pytesseract
-from PIL import Image, ImageFilter
 # from scipy.ndimage.filters import gaussian_filter
 # import numpy
-# from selenium import webdriver
-# from selenium.webdriver.common.by import By
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import pytesseract
 import time
+import cv2
+
+from PIL import Image, ImageFilter
 
 
 def main():
-    # driver = webdriver.Chrome()
-    # driver.get("https://youtu.be/KcVHmlYsjnY?t=728")
-    # time.sleep(5)
-    # elem = driver.find_element(By.XPATH, '//*[@id="movie_player"]')
-    # time.sleep(2)
-    # elem.screenshot("yo.png")
+    driver = webdriver.Chrome()
+    driver.get("https://youtu.be/KcVHmlYsjnY?t=728")
+    time.sleep(5)
+    elem = driver.find_element(By.XPATH, '//*[@id="movie_player"]')
+    elem.click()
+    time.sleep(0.4)
+    elem.click()
+    time.sleep(.5)
+    elem.send_keys("f")
+    elem.screenshot("yo.png")
     origin = Image.open("yo.png")
     black_and_white = origin.convert("L")
     threshold = black_and_white.point(lambda p: p > 100 and 255)
     threshold = threshold.filter(ImageFilter.EDGE_ENHANCE_MORE)
     threshold = threshold.filter(ImageFilter.SHARPEN)
     threshold = threshold.filter(ImageFilter.SMOOTH_MORE)
+    threshold = threshold.filter(ImageFilter.FIND_EDGES)
+    w, h = threshold.size
+    threshold = threshold.crop((580, 120, w-20, h-170))
     threshold.save("wow.png")
     time.sleep(.2)
     cropped = Image.open("wow.png")
-    w, h = cropped.size
-    wow = cropped.crop((580, 120, w, h-140))
-    wow.save("out.png")
+    print(pytesseract.image_to_string(cropped))
+
 
 if __name__ == '__main__':
     main()
